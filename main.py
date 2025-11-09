e5fe7a38b1137ad00a3a6976c543640a8076cece
zakari1231 <zakari_1231@hotmail.fr>
2025-11-09 21:50:49 +0100

    add fucntion

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ import os
 app = FastAPI(title="GTU Student Credential API")
 
 DATA_PATH = "data.csv"
+
+def load_data():
+    if not os.path.exists(DATA_PATH):
+        raise FileNotFoundError("data.csv file not found.")
+    return pd.read_csv(DATA_PATH)
+
+def find_student(firstname: str, lastname: str, num_inscreption: str):
+    data = load_data()
+    match = data[
+        (data["firstname"].str.lower().str.strip() == firstname.lower().strip()) &
+        (data["lastname"].str.lower().str.strip() == lastname.lower().strip()) &
+        (data["num_inscreption"].astype(str).str.strip() == str(num_inscreption).strip())
+    ]
+    return match
